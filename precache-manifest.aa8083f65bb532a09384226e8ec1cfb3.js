f55c6a68c3a3f612ed057e8d71f7c8fdf6f4689a
Allegra <aukseallegra@gmail.com>
2019-07-12 21:15:56 +0100

    Updates

---
+++ precache-manifest.aa8083f65bb532a09384226e8ec1cfb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4da26e73594c827e60c7",
+    "url": "/world-map/static/js/runtime~main.11fe7b17.js"
+  },
+  {
+    "revision": "40d830d3750c41cca0ee",
+    "url": "/world-map/static/js/main.c2e0b2fa.chunk.js"
+  },
+  {
+    "revision": "ace4e92345cadc56f5d4",
+    "url": "/world-map/static/js/2.9d931f7b.chunk.js"
+  },
+  {
+    "revision": "40d830d3750c41cca0ee",
+    "url": "/world-map/static/css/main.e25c3da4.chunk.css"
+  },
+  {
+    "revision": "aaefbb37db65ec8e3d88a354a0773cd7",
+    "url": "/world-map/index.html"
+  }
+];
